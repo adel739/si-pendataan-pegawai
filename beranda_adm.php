394618fb785dbd245ebf5b6bdb4c0ab079d02c42
adel739 <78077866+adel739@users.noreply.github.com>
2021-01-27 17:10:31 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">SISTEM INFORMASI DI RUMAH SAKIT UMUM DAERAH H. ABDUL MANAN SIMATUPANG KISARAN</h3>
+                </div>
+                <div class="panel-body">
+                     <thead>
+                          <p align="center"><img src="img/logo.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>RSUD HAMS KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di Beranda Sistem Informasi Data Pegawai RSUD HAMS Kisaran.<br>
+                            RSUD HAMS Kisaran merupakan Rumah Sakit Umum Daerah Tipe C yang ada di kota Kisaran.<br>
+                            Kabupaten Asahan Provinsi Sumatera Utara yang merupakan sebuah Badan Layanan Umum<br>
+                            dan diselenggarakan oleh Pemerintah Daerah Kabupaten Asahan. Melalui Halaman ini <br>
+                            kami dapat melakukan Pendataan Pegawai di Rumah Sakit, Data Akurat Rumah Sakit,<br>
+                            dan juga Pengelolaan Laporan bagian - bagian Bidang di Rumah Sakit.<br><br>
+                            Akses Menu Master Data pada bagian atas sistem untuk Pendataan Pegawai dan Masa Pegawai Menjabat.<br>
+                            Untuk mengelola/mencetak Laporan, dapat dilakukan dengan mengakses Menu Report pada bagian atas sistem.<br>
+                            Akses Menu User untuk mengelola Informasi mengenai user yang Login ke sistem.<br><br><br>
+                          </p>
+                         </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
